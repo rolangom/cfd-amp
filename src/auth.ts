e2c13382244c2f48366053c21b70bfb5a38e40c1
Rolando Gómez Tabar <rolangom@gmail.com>
2024-09-04 23:32:57 -0600

    removed unimportant line

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,7 @@
 import NextAuth from "next-auth"
 import { DrizzleAdapter,  } from "@auth/drizzle-adapter"
 // @ts-ignore
-import { DefaultPostgresSchema } from "@auth/drizzle-adapter/src/lib/pg"
+// import { DefaultPostgresSchema } from "@auth/drizzle-adapter/src/lib/pg"
 import { db } from "./db"
 import { users, accounts } from "../schemas/users"
 import authConfig from "./auth.config"
@@ -12,6 +12,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db, {
     usersTable: users,
     accountsTable: accounts,
+    // @ts-ignore
   } as DefaultPostgresSchema),
   session: {
     strategy: "jwt",
